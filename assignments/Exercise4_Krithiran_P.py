08e04f6a27db988cf5955bfdc141cec2d6661356
kid <nonstop5551@gmail.com>
2025-04-25 15:35:26 +0700

    Update Exercise4_Krithiran_P.py

--- assignments/Exercise4_Krithiran_P.py
+++ assignments/Exercise4_Krithiran_P.py
@@ -1,1 +1,10 @@
-Foundation_English = float(input("your score in Foundation English"))
+Foundation_English = float(input("your score in Foundation English "))
+General_Business = float(input("your score in General Business "))
+Introduction_to_Computer_Systems = float(input("your score in Introduction to Computer Systems "))
+Computer_Programming = float(input("your score in Computer Programming "))
+
+print("----- Your Score -----")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer Systems :",Introduction_to_Computer_Systems)
+print("Computer Programming :",Computer_Programming)
