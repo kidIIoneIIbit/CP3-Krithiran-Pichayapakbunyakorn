3776f8298b952668c615605f61645db9daa5f26f
kid <nonstop5551@gmail.com>
2025-04-29 21:48:06 +0700

    add 73

--- assignments/Lecture73.py
+++ assignments/Lecture73.py
@@ -1,18 +1,18 @@
+systemMenu = {"fish":100,"pork":200,"bird":500}
 menulist = []
 while True:
     menuname = input("Please Enter Menu :")
     if (menuname.lower()== "exit"):
         break
     else:
-        menuprice = int(input("Price : "))
-        menulist.append([menuname,menuprice])
+        menulist.append([menuname,systemMenu[menuname]])
 
 
 def showBill():
     total = 0
     print("-----My food -----")
     for i in range(len(menulist)):
-        print(menulist[i])
+        print(menulist[i][0],menulist[i][1])
         total += menulist[i][1]
     print("total : ",total)
 print(menulist)
